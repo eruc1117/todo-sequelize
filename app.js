315ef2ef273b5e3fa1278a9afaa0e498f2087569
eruc <eruc101010@gmail.com>
2022-04-15 21:01:31 +0800

    feat: add users route and related page

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
+const routes = require('./routes')
 const PORT = 3000
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
+app.use(routes)
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
