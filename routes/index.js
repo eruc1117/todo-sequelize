96974575c9ccc5b2afa0340cd3f5db8d9ea28a9c
eruc <eruc101010@gmail.com>
2022-04-15 22:11:14 +0800

    feat: add index page and related function

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const users = require('./models/users')
+const index = require('./models/index')
 
 router.use('/users', users)
+router.use('/', index)
 
 module.exports = router
