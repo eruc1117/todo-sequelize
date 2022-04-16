669ee241a93bc8623bc86aa637191160d7911e6d
eruc <eruc101010@gmail.com>
2022-04-16 11:23:04 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const { authenticator } = require('../middleware/auth')
 const users = require('./models/users')
 const index = require('./models/index')
 const todos = require('./models/todo')
+const auth = require('./models/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/todos', authenticator, todos)
 router.use('/', authenticator, index)
 
