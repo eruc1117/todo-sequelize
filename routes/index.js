fffd14f0aa9002f7b28c04dc2d7c524bbf97ecf3
eruc <eruc101010@gmail.com>
2022-04-16 09:55:30 +0800

    feat: add read todo detail function

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const users = require('./models/users')
 const index = require('./models/index')
+const todos = require('./models/todo')
 
 router.use('/users', users)
+router.use('/todos', todos)
 router.use('/', index)
 
 module.exports = router
