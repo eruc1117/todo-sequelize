93cd8443c0255196004bba92c52f80b380f2dfb3
eruc <eruc101010@gmail.com>
2022-04-16 11:53:31 +0800

    feat: add update todo function

--- routes/models/todo.js
+++ routes/models/todo.js
@@ -15,8 +15,27 @@ router.post('/new', (req, res) => {
     updatedAt: new Date(),
     createdAt: new Date()
   }
-  return Todo.create( name )
+  return Todo.create(name)
     .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then((todo) => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.update({ name }, {
+    where: {
+      id
+    }
+  })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
